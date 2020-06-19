c542bba4090f0a9d881d2e132d6e433f5b78f6a3
fabio.mota <fabio.mota@hostelworld.com>
2020-06-19 15:51:37 +0100

    removed redundant code

--- app/controllers/player.controller.js
+++ app/controllers/player.controller.js
@@ -1,36 +1,6 @@
 const db = require("../models");
 const Player = db.players;
 
-// Create and Save a new Player
-exports.create = (req, res) => {
-
-};
-
-// Retrieve all Players from the database.
-exports.findAll = (req, res) => {
-
-};
-
-// Find a single Player with an id
-exports.findOne = (req, res) => {
-
-};
-
-// Update a Player by the id in the request
-exports.update = (req, res) => {
-
-};
-
-// Delete a Player with the specified id in the request
-exports.delete = (req, res) => {
-
-};
-
-// Delete all Player from the database.
-exports.deleteAll = (req, res) => {
-
-};
-
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.name || !req.body.position) {
